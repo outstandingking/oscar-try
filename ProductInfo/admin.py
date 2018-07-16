db7fc1c13b6404ff3510db59205744ec928dd355
jojo.J.wang <wangjiaojiao640801@gmail.com>
2018-07-16 21:16:32 +0800

    fix login issues

---
+++ ProductInfo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import ProductOwner
+
+
+admin.site.register(ProductOwner)
---
+++ senderConfirmInfo/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import ConfirmOrderInfo, confirmProductRelathionship
+
+admin.site.register(ConfirmOrderInfo)
+admin.site.register(confirmProductRelathionship)
